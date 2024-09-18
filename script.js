5954da523a6d9383c39896c11ce380e92b55f9c8
QingdaHu <106006865+Teriiii1@users.noreply.github.com>
2024-09-18 14:54:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('网页已加载！');
 });
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const tag = document.getElementById('blog-tag');
+        tag.addEventListener('click', function() {
+            window.location.href = 'blog.html';
+        });
+    });
+</script>
